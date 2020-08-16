5f1f87ff53a59c7b270354b275d96f6a0d61a2a9
Ashwin V C <ashwinvc@microsoft.com>
2020-08-16 23:23:03 +0530

    Completed module 3

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -25,6 +25,11 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
+
+
+    // ***
+    // TODO - Task 2 - Launch the Google Maps Activity
+    // ***
     public void createMapIntent(View view) {
         Uri location = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
@@ -35,10 +40,12 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
-
-    // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        Uri phoneNumber = Uri.parse("tel:0123456789");
+        phoneIntent.setData(phoneNumber);
+        startActivity(phoneIntent);
+    }
 }
